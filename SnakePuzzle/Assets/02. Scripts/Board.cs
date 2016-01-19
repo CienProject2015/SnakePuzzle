283839711f5980afb2230468179a21132a29fe60
olympiostar <olympiostar@gmail.com>
2016-01-20 00:25:12 +0900

    XML 스키마 수정으로 Size 정보 추가 및 XML DB Load 스크립트 분리

--- SnakePuzzle/Assets/02. Scripts/Board.cs
+++ SnakePuzzle/Assets/02. Scripts/Board.cs
@@ -25,27 +25,20 @@ public class Board{
     private int board_Height;
     private int board_Width;
     private string tempColor;
+	private DB_Loader db_loader= new DB_Loader();
 
     public Board(){
-        board_Height = 12;
-        board_Width = 12;
-        Tile = new string[board_Height, board_Width];
-		loadTileDB ();
+		Tile = db_loader.get_tile();
     }
 
     public Board(int a, int b){
-        board_Height = a;
-        board_Width = b;
-        Tile = new string[board_Height, board_Width];
-		loadTileDB ();
-
+		Tile = db_loader.get_tile();
     }
 
     public string[,] getTile(){
         return Tile;
     }
-
-
+		
 	/*public string getRndColor(){
         string retString = "";
         int num = Random.Range(0,3);
@@ -71,50 +64,6 @@ public class Board{
 
         return retString;
     }*/
-
-
-	private void loadTileDB(){
-		XmlDocument xmldoc = new XmlDocument ();
-		xmldoc.Load ("DB.xml");
-		XmlElement root = xmldoc.DocumentElement;
-		XmlNodeList nodes = root.ChildNodes;
-
-		int x, y;
-		string text;
-		string[] parse;
-		string[] tile_list = { "Spawn", "Red","Yellow","Green","Blue","Purple"};
-
-		foreach (XmlNode node in nodes) {
-			for(int i = 0 ; i < tile_list.Length; i++){
-				text = node.SelectSingleNode(tile_list[i]).InnerText;
-				if (text != "null"){
-					parse = node.SelectSingleNode (tile_list[i]).InnerText.Split (',');
-					x = XmlConvert.ToInt32 (parse [1]);
-					y = XmlConvert.ToInt32 (parse [2]);
-					switch(parse[0]){
-					case "U":
-						Tile [x, y] = "SpawnUp";
-						break;
-					case "D":
-						Tile [x, y] = "SpawnDown";
-						break;
-					case "R":
-						Tile [x, y] = "SpawnRight";
-						break;
-					case "L":
-						Tile [x, y] = "SpawnLeft";
-						break;
-					case "0":
-						Tile [x, y] = tile_list[i];
-						break;
-					default :
-						break;
-					}
-				}
-			}// end for 
-		}// end foreach
-		Tile[0, 0] = Tile[0, board_Height-1] = Tile[board_Width-1, 0] = Tile[board_Width-1, board_Height-1] = "NULL";
-	}
 }
 
 
